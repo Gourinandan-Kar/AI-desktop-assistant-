832b81ad267b111d8f49de75d204d632ff9d54d5
Gourinandan-Kar <75492281+Gourinandan-Kar@users.noreply.github.com>
2021-05-03 13:37:29 +0530

    Add files via upload
    Core file

---
+++ project.py
@@ -0,0 +1,82 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-us')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('gourinandan99@gmail.com', 'your-password')
+    server.sendmail('kar254149@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=4)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'ipl score' in query:
+            webbrowser.open("ipl live score")
+        elif 'open facebook' in query:
+            webbrowser.open("facebook.com")
+        elif 'open twitter' in query:
+            webbrowser.open("twitter.com")           
+        elif 'play music' in query:
+            music_dir = 'D:\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open vscode' in query:
+            codePath = "C:\\Users\\LENOVO\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to ram' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "gourinandan99@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir. I am not able to send this email")  
